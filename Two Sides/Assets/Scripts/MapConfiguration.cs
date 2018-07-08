ac6dab5a39130a413a80e88b46af7507cfcfc90c
Kristian-PC\Kristian <kristian.leiner@yahoo.com>
2018-07-08 11:21:00 +0200

    Prototype of a procedural map generation
    Basic prototype

---
+++ Two Sides/Assets/Scripts/MapConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapConfiguration : MonoBehaviour {
+
+    public GameObject straight;
+    public GameObject left;
+    public int leftAmount;
+    public GameObject right;
+    public int rightAmount;
+
+    private Vector3 map;
+    private int straightAmount;
+
+    void Awake()
+    {
+        straightAmount = Random.Range((leftAmount + rightAmount * 2), (leftAmount + rightAmount * 6));
+    }
+
+    public int ReturnStraightAmount()
+    {
+        return straightAmount;
+    }
+
+
+}
---
+++ Two Sides/Assets/Scripts/ProceduralGeneration.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralGeneration : MonoBehaviour {
+
+    public GameObject straight;
+    public GameObject turn;
+    public int turnAmount;
+
+    private Vector3 map;
+    private int straightAmount;
+
+    private float positionX = 0;
+    private float positionZ = 0;
+    private int lookPosition = 0;
+
+    private float turnRotation = 0;
+    private float straightRotation = 0;
+
+    private enum Look { PlusX, MinusX, PlusZ, MinusZ }
+
+    void Awake()
+    {
+        InstantiateStraights();
+        InstantiateTurns();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+	}
+
+    void InstantiateStraights()
+    {
+        straightAmount = Random.Range(1, 10);
+
+        for (int i = 0; i < straightAmount; i++)
+        {
+            if (lookPosition == (int)Look.PlusX)
+            {
+                if (i == 0) positionX += 3.5f;
+                else positionX += 5;
+            }
+            if (lookPosition == (int)Look.MinusX)
+            {
+                if (i == 0) positionX -= 3.5f;
+                else positionX -= 5;
+            }
+                
+            if (lookPosition == (int)Look.PlusZ)
+            {
+                if (i == 0) positionZ += 3.5f;
+                else positionZ += 5;
+            }
+            if (lookPosition == (int)Look.MinusZ)
+            {
+                if (i == 0) positionZ -= 3.5f;
+                else positionZ -= 5;
+            }
+
+            Instantiate(straight, new Vector3(positionX, 0, positionZ), Quaternion.Euler(new Vector3(0, straightRotation, 0)));
+        }
+    }
+
+    void InstantiateTurns()
+    {
+        for (int i = 0; i < turnAmount; i++)
+        {
+            if (lookPosition == (int)Look.PlusX)
+            {
+                positionX += 3.5f;
+                
+                if (Random.value < 0.5)
+                {
+                    turnRotation = 90;
+                    lookPosition = 3;
+                }
+                else
+                {
+                    turnRotation = 180;
+                    lookPosition = 2;
+                }
+                straightRotation = 90;
+            }
+            else if (lookPosition == (int)Look.MinusX)
+            {
+                positionX -= 3.5f;
+
+                if (Random.value < 0.5)
+                {
+                    turnRotation = 0;
+                    lookPosition = 3;
+                }
+                else
+                {
+                    turnRotation = -90;
+                    lookPosition = 2;
+                }
+                straightRotation = 90;
+            }
+            else if (lookPosition == (int)Look.PlusZ)
+            {
+                positionZ += 3.5f;
+
+                if (Random.value < 0.5)
+                {
+                    turnRotation = 0;
+                    lookPosition = 0;
+                }
+                else
+                {
+                    turnRotation = 90;
+                    lookPosition = 1;
+                }
+                straightRotation = 0;
+            }
+            else
+            {
+                positionZ -= 3.5f;
+
+                if (Random.value < 0.5)
+                {
+                    turnRotation = -90;
+                    lookPosition = 0;
+                }
+                else
+                {
+                    turnRotation = 180;
+                    lookPosition = 1;
+                }
+                straightRotation = 0;
+
+            }
+
+            Instantiate(turn, new Vector3(positionX, 0, positionZ), Quaternion.Euler(new Vector3(0, turnRotation, 0)));
+
+            InstantiateStraights();
+        }
+    }
+}
